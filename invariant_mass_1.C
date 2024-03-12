df58e27fa97f404db70a16978ddb30c60a7254d2
feldanius <43442037+feldanius@users.noreply.github.com>
2024-03-12 17:04:32 -0600

    Add files via upload

---
+++ invariant_mass_1.C
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TLorentzVector.h"
+
+using namespace std;
+
+int invariant_mass_1() {
+    // Nombres de los archivos ROOT
+    vector<string> filenames = {
+        "events_008995949.root", "events_034459462.root", "events_067618566.root", "events_088898158.root",
+        "events_107858353.root", "events_158628525.root", "events_177743949.root", "events_192707663.root",
+        "events_019839222.root", "events_061640142.root", "events_071153275.root", "events_090014287.root",
+        "events_129556080.root", "events_174244458.root", "events_178769856.root", "events_027985022.root",
+        "events_065244791.root", "events_075207014.root", "events_090844778.root", "events_138935081.root",
+        "events_176415634.root", "events_180776309.root"
+    };
+
+    // Crear un histograma para la masa invariante
+    TH1F* hist_invariant_mass = new TH1F("hist_invariant_mass", "Masa invariante de los jets", 100, 0, 200);
+
+    // Iterar sobre los archivos
+    for (const auto& filename : filenames) {
+        // Abrir archivo ROOT
+        TFile* file = TFile::Open(filename.c_str());
+        if (!file) {
+            cerr << "Error al abrir el archivo " << filename << endl;
+            continue;
+        }
+
+        // Obtener el árbol de eventos
+        TTree* tree = (TTree*)file->Get("events");
+        if (!tree) {
+            cerr << "No se pudo encontrar el árbol de eventos en " << filename << endl;
+            file->Close();
+            continue;
+        }
+
+        // Variables para almacenar la información de los jets
+        vector<float> jet_energy(2);
+        vector<float> jet_px(2);
+        vector<float> jet_py(2);
+        vector<float> jet_pz(2);
+
+        // Conectar las variables del árbol con las variables locales
+        tree->SetBranchAddress("Jet.energy", &jet_energy[0]);
+        tree->SetBranchAddress("Jet.momentum.x", &jet_px[0]);
+        tree->SetBranchAddress("Jet.momentum.y", &jet_py[0]);
+        tree->SetBranchAddress("Jet.momentum.z", &jet_pz[0]);
+
+        // Iterar sobre los eventos
+        for (Long64_t i = 0; i < tree->GetEntries(); ++i) {
+            tree->GetEntry(i);
+
+            // Crear los vectores Lorentz de los jets
+            vector<TLorentzVector> jets;
+            for (int j = 0; j < 2; ++j) {
+                TLorentzVector jet;
+                jet.SetPxPyPzE(jet_px[j], jet_py[j], jet_pz[j], jet_energy[j]);
+                jets.push_back(jet);
+            }
+
+            // Calcular la masa invariante
+            TLorentzVector dijet = jets[0] + jets[1];
+            float invariant_mass = dijet.M();
+
+            // Llenar el histograma con la masa invariante
+            hist_invariant_mass->Fill(invariant_mass);
+        }
+
+        // Cerrar el archivo ROOT
+        file->Close();
+    }
+
+    // Guardar el histograma en un archivo ROOT
+    TFile* output_file = TFile::Open("invariant_mass_histogram.root", "RECREATE");
+    if (output_file) {
+        hist_invariant_mass->Write();
+        output_file->Close();
+    } else {
+        cerr << "Error al abrir el archivo de salida" << endl;
+    }
+
+    // Limpiar la memoria
+    delete hist_invariant_mass;
+
+    return 0;
+}
+
