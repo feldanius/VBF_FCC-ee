2ac5fc88cb48160325f5a5c9bf32ca4553a50c65
feldanius <43442037+feldanius@users.noreply.github.com>
2024-05-28 02:23:56 -0600

    Create cutinvmass_invariant_mass_bg_zh_mumuh.C

---
+++ cutinvmass_invariant_mass_bg_zh_mumuh.C
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TLorentzVector.h"
+
+using namespace std;
+
+int cutinvmass_invariant_mass_bg_zh_mumuh() {
+    // Nombres de los archivos ROOT
+    vector<string> filenames = {
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_023261681.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_034927544.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_058572739.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_073291643.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_092281869.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_189404834.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_033570460.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_036028207.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_068513877.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_075232335.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_183713990.root",
+"/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_mumuH_ecm365/events_197337122.root"
+    };
+
+    // Crear un histograma para la masa invariante
+    TH1F* hist_invariant_mass = new TH1F("hist_invariant_mass", "Masa invariante de los jets", 100, 0, 400);
+
+    // Iterar sobre los archivos
+    for (const auto& filename : filenames) {
+        // Abrir archivo ROOT
+        TFile* file = TFile::Open(filename.c_str());
+        if (!file) {
+            cerr << "Error al abrir el archivo " << filename << endl;
+            continue;
+        }
+
+        // Obtener el árbol de eventos
+        TTree* tree = (TTree*)file->Get("events");
+        if (!tree) {
+            cerr << "No se pudo encontrar el árbol de eventos en " << filename << endl;
+            file->Close();
+            continue;
+        }
+
+        // Variables para almacenar la información de los jets
+        vector<float> jet_energy(2);
+        vector<float> jet_px(2);
+        vector<float> jet_py(2);
+        vector<float> jet_pz(2);
+
+        // Conectar las variables del árbol con las variables locales
+        tree->SetBranchAddress("Jet.energy", &jet_energy[0]);
+        tree->SetBranchAddress("Jet.momentum.x", &jet_px[0]);
+        tree->SetBranchAddress("Jet.momentum.y", &jet_py[0]);
+        tree->SetBranchAddress("Jet.momentum.z", &jet_pz[0]);
+
+        // Iterar sobre los eventos
+        for (Long64_t i = 0; i < tree->GetEntries(); ++i) {
+            tree->GetEntry(i);
+
+            // Crear los vectores Lorentz de los jets
+            vector<TLorentzVector> jets;
+
+            for (int j = 0; j < 2; ++j) {
+                TLorentzVector jet;
+                jet.SetPxPyPzE(jet_px[j], jet_py[j], jet_pz[j], jet_energy[j]);
+                jets.push_back(jet);
+            }
+
+            // Verificar que haya al menos dos jets
+            if (jets.size() < 2) continue;
+
+            // Calcular la masa invariante de los dos jets
+            TLorentzVector dijet = jets[0] + jets[1];
+            float invariant_mass = dijet.M();
+
+            // Verificar el corte de la masa invariante
+            const float mH = 125.0;
+            if (fabs(invariant_mass - mH) >= 30.0) {
+                // Llenar el histograma con la masa invariante
+                hist_invariant_mass->Fill(invariant_mass);
+            }
+        }
+
+        // Cerrar el archivo ROOT
+        file->Close();
+    }
+
+    // Guardar el histograma en un archivo ROOT
+    TFile* output_file = TFile::Open("cut_invmass_invariant_mass_jets_histogram_bg_zh_mumuh_400.root", "RECREATE");
+    if (output_file) {
+        hist_invariant_mass->Write();
+        output_file->Close();
+    } else {
+        cerr << "Error al abrir el archivo de salida" << endl;
+    }
+
+    // Limpiar la memoria
+    delete hist_invariant_mass;
+
+    return 0;
+}
