a6155658af0b3dd1ddea6be8cc9d084f8cc69405
feldanius <43442037+feldanius@users.noreply.github.com>
2024-06-20 11:00:44 -0600

    Create JET_signal_mass.C

---
+++ JET_signal_mass.C
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+
+int JET_signal_mass() {
+    // Vector de archivos ROOT
+    std::vector<std::string> filenames = {
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_008995949.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_034459462.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_067618566.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_088898158.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_107858353.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_158628525.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_177743949.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_192707663.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_019839222.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_061640142.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_071153275.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_090014287.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_129556080.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_174244458.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_178769856.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_027985022.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_065244791.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_075207014.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_090844778.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_138935081.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_176415634.root",
+        "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/wzp6_ee_nunuH_ecm365/events_180776309.root"
+    };
+
+    // Crear un histograma para la energía perdida
+    TH1F* hist = new TH1F("missingET", "Missing Transverse Energy;E_{T}^{miss} (GeV);Events", 100, 0, 400);
+
+    // Bucle sobre los archivos
+    for (const auto& filename : filenames) {
+        // Abre el archivo ROOT
+        TFile* file = TFile::Open(filename.c_str());
+        if (!file || file->IsZombie()) {
+            std::cerr << "Error abriendo el archivo: " << filename << std::endl;
+            continue;
+        }
+
+        // Obtener el árbol "events"
+        TTree* tree = (TTree*)file->Get("events");
+        if (!tree) {
+            std::cerr << "Error obteniendo el árbol 'events' del archivo: " << filename << std::endl;
+            file->Close();
+            continue;
+        }
+
+        // Variable para almacenar la energía perdida
+        float missingEnergy;
+
+        // Configurar la dirección de la rama
+        tree->SetBranchAddress("Jet.mass", &missingEnergy);
+
+        // Bucle sobre los eventos
+        Long64_t nentries = tree->GetEntries();
+        for (Long64_t i = 0; i < nentries; i++) {
+            tree->GetEntry(i);
+            hist->Fill(missingEnergy);
+        }
+
+        // Cerrar el archivo
+        file->Close();
+    }
+
+    // Crear un archivo ROOT para guardar el histograma
+    TFile* outFile = new TFile("JET_signal_mass.root", "RECREATE");
+    hist->Write();
+    outFile->Close();
+
+    // Limpieza
+    delete hist;
+    delete outFile;
+
+    return 0;
+}
